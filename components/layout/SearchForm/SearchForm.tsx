c43214a009c369c0a2373e70de953f23c71ad37f
kondrse <kondor85@mail.ru>
2026-03-01 00:35:22 +0300

    remove from search

--- components/layout/SearchForm/SearchForm.tsx
+++ components/layout/SearchForm/SearchForm.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react'
 import { useDebounce } from '@/lib/hooks/useDebounce'
 import { observer } from 'mobx-react-lite'
 import { rootStore } from '@/lib/store/rootStore'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 export default observer(
 function SearchForm() {
@@ -12,11 +14,16 @@ function SearchForm() {
   const debouncedSearchQuery = useDebounce(inputValue)
 
   useEffect(() => {
-    store.setSearchQuery(debouncedSearchQuery)
+    store.setSearchQuery(debouncedSearchQuery.trim())
   }, [debouncedSearchQuery, store])
 
+  const handleClear = () => {
+    setInputValue('')
+    store.setSearchQuery('')
+  }
+
   return (
-    <div className="w-[1000px]">
+    <div className="w-[300px] md:w-[1000px] relative">
       <input
         type="text"
         className="bg-gray3 border border-gray3 rounded px-5 py-2 w-full"
@@ -24,6 +31,11 @@ function SearchForm() {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
+      {inputValue.trim() && <FontAwesomeIcon
+        icon={faTimes}
+        className="text-gray-500 hover:text-[var(--color-primary)] absolute top-3 right-3 cursor-pointer"
+        onClick={handleClear}
+      />}
     </div>
   )
 })
