0c2c31ce757b8ffe57f798bdb83d0c8fb09947ca
kondrse <kondor85@mail.ru>
2026-03-01 13:39:39 +0300

    fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ import { LoginPanel } from "@/shared/ui/LoginPanel";
 export default function LoginPage() {
   return (
     <LoginPanel>
-      <Logo className="m-auto mb-5s"/>
+      <Logo className="m-auto mb-5"/>
       <h1 className="text-[40px] text-center">Добро пожаловать!</h1>
       <p className="text-xl text-center">Пожалуйста, авторизируйтесь</p>
       <LoginForm />
